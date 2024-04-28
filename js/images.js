0743b71e704194a0cfda23e6730ec53e14bd3d7a
Mr-By2e <54022789+Mr-By2e@users.noreply.github.com>
2024-04-28 18:56:21 +0500

    Add files via upload

---
+++ js/images.js
@@ -0,0 +1,23 @@
+
+
+// Fetch the JSON data
+fetch('js\\images.json')
+  .then(response => response.json())
+  .then(data => displayImages(data))
+  .catch(error => console.error('Error fetching data:', error));
+
+// Display the images
+function displayImages(data) {
+  const gallery = document.getElementById('gallery');
+  data.forEach(item => {
+    const div = document.createElement('div');
+    div.className = 'gallery-item';
+    div.innerHTML = `
+      <a href="${item.url}" target="_blank">
+        <img src="${item.url}" alt="${item.description}" class="thumbnail">
+        <div class="caption">${item.description}</div>
+      </a>
+    `;
+    gallery.appendChild(div);
+  });
+}
